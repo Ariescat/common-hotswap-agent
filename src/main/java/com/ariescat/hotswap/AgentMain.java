c51060af84dba6ee8bb113d160975914332d4b85
Ariescat <1275563227@qq.com>
2020-01-10 15:30:37 +0800

    feat(hotswap): jdk动态编译 初始提交

---
+++ src/main/java/com/ariescat/hotswap/AgentMain.java
@@ -0,0 +1,14 @@
+package com.ariescat.hotswap;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * @author Ariescat
+ * @version 2020/1/10 9:57
+ */
+public class AgentMain {
+
+    public static void agentmain(String args, Instrumentation inst) {
+
+    }
+}
